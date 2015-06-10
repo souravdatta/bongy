7130276c76ca8a70cfa82a70d68fd6471e05cf94
souravdatta <soura.jagat@gmail.com>
2015-06-10 21:08:30 +0530

    Incremental Changes (GUI) 10

--- static/index.js
+++ static/index.js
@@ -4,16 +4,28 @@ Under MIT License. Please see accompanying LICENSE document.
 
 (function () {
     $(document).ready(function () {
+        var loader = $('#loader');
+        loader.addClass('hide');
+
         $('#converter').click(function (){
-            var text = $('#eng').html();
+            var text = $('#eng').html().trim();
+            if (text == '') return;
             var dest = $('#bng');
+
+            loader.removeClass('hide');
+            loader.addClass('show');
+
             $.ajax(
                 ('/api/v1/' + text),
                 {
                     success: function (d, status, req) {
                         dest.html(d);
+                        loader.removeClass('show');
+                        loader.addClass('hide');
                     },
                     error: function () {
+                        loader.removeClass('show');
+                        loader.addClass('hide');
                         console.log('Error retrieving result from server...')
                     }
                 }
